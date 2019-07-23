5d7dac22d9358a2b999a37858788d71e19b0c3a9
ElijahGaft <gaft.ilya.m@yandex.ru>
2019-07-23 18:32:19 +0300

    Работает но не закончил

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-# Задание:
-# Вывести список групп в ВК в которых состоит пользователь, но не состоит никто из его друзей. В качестве жертвы, на ком тестировать, можно использовать: https://vk.com/eshmargunov
-#
-# Входные данные:
-# Имя пользователя или его id в ВК, для которого мы проводим исследование.
-#
-# Внимание: и имя пользователя (eshmargunov) и id (171691064) - являются валидными входными данными.
-#
-# Ввод можно организовать любым способом:
-#
-# из консоли
-# из параметров командной строки при запуске
-# из переменной
-# Выходные данные:
-# Файл groups.json в формате:
-
-
-
+import sys
+import progressbar
+import time
 from urllib.parse import urlencode
 import requests
+from pip._vendor.msgpack.fallback import xrange
 
 APP_ID = 7058177     # ID моего приложения зарегестрированного на сайте VK
 AUTH_URL = 'https://oauth.vk.com/authorize'
@@ -25,7 +11,7 @@ Auth_DATA = {
     'client_id' : APP_ID,
     'display' : 'page',
     'scope' : 'status',
-    'response_type' : 'token',
+    'response_type' : 'token'
 }
 # print('?'.join((AUTH_URL, urlencode(Auth_DATA)))) # выдаёт ссылку на страничку разрешений прав, так я могу узнать токен.
 
@@ -40,9 +26,9 @@ class User:
         return {
             'access_token' : self.token,
             'v' : '5.25',
-            'user_id' : self.user_id,
-            'fields' : 'nickname'
+            'user_id' : self.user_id
         }
+
 
     def request(self, method, params): # Формирование запроса
         response = requests.get(
@@ -60,10 +46,61 @@ class User:
         )
         return response.json()['response']
 
+    def get_groups(self):
+        params = self.get_params()
+        response = self.request(
+            'groups.get',
+            params=params
+        )
+        return response.json()['response']
 
-Evgeniy = User(306008470, TOKEN)
+def User_group_analise(id):
+    try:
+        FRIEND = User(id, TOKEN)
+        user_group = FRIEND.get_groups()
+        user_group = user_group.get('items')
+        print('.', end='')
+        return user_group
+    except:
+        pass
+
+def Same_group(user1_list, user2_list):
+    try:
+        set(user2_list).difference(set(user1_list))
+    except:
+        pass
+    return user2_list
+
+
+def show_progress(value):
+    sys.stderr.write('%d\r' % value)
+
+ILYA = User('171691064', TOKEN)
 # с помощью метода класса создаём список групп юзера
-Friends = Evgeniy.get_friends()# метод класса для поиска друзей юзера, переменная хранит словарь (имя и id)
+friends = ILYA.get_friends() # метод класса для поиска друзей юзера, переменная хранит словарь (имя и id)
+friends = friends.get('items')
+groups = ILYA.get_groups()
+groups = groups.get('items')
+counter = 0
+print(groups)
+# for i in range(1, len(friends)):
+#     sys.stdout.write('\r')
+#     part = float(i)/(len(friends)-1)
+#     symbols_num = int(100 * part)
+#     sys.stdout.write("[%-100s] %3.2f%%" % ('='*symbols_num, part*100))
+#     sys.stdout.flush()
+#     time.sleep(0.01)
+for user_id in friends:
+    groups_list = User_group_analise(str(user_id))
+    groups = Same_group(groups_list, groups)
+    sys.stdout.write('\r')
+    part = float(counter)/len(friends)
+    symbols_num = int(100 * part)
+    sys.stdout.write("[%-100s] %3.2f%%" % ('=' * symbols_num, part * 100))
+    sys.stdout.flush()
+    time.sleep(0.01)
+    counter += 1
+print('\n')
 # для каждого друга применяем метод поиска груп, сравниваем с групами нашего юзера и удаляем из его списка, если находит повторения.
 # ВОПРОС: Смогу ли я применять метод класа юзера на его друзьях, или сначала для каждого друга мне по id нужно создать свой обьект класса?
-print(Friends)
+print(groups)
